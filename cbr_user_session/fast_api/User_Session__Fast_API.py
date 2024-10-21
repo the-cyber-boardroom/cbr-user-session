5aac330145ccd7d0b21912f87fabd165b840a8f0
Dinis Cruz <dinis.cruz@owasp.org>
2024-10-21 16:46:31 +0100

    started simple version of User_Session__Config

--- cbr_user_session/fast_api/User_Session__Fast_API.py
+++ cbr_user_session/fast_api/User_Session__Fast_API.py
@@ -1,3 +1,6 @@
+from osbot_local_stack.local_stack.Local_Stack import Local_Stack
+
+from cbr_user_session.User_Session__Config import user_session__config
 from cbr_user_session.fast_api.routes.Routes__Session   import Routes__Session
 from osbot_fast_api.api.Fast_API                        import Fast_API
 from cbr_user_session.fast_api.routes.Routes__Info      import Routes__Info
@@ -7,6 +10,16 @@ class User_Session__Fast_API(Fast_API):
     base_path      : str  = '/user-session'
     enable_cors    : bool = True
     default_routes : bool = False
+
+    def setup(self):
+        user_session__config.setup()
+        self.setup__local_stack()
+        super().setup()
+        return self
+
+    def setup__local_stack(self):
+        if user_session__config.use_local_stack:
+            Local_Stack().activate()
 
     def setup_routes(self):
         self.add_routes(Routes__Info   )
