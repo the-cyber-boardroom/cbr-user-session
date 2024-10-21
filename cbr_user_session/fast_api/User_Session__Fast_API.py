5b0426ad90e3c91c455dd756845d94a865c47804
Dinis Cruz <dinis.cruz@owasp.org>
2024-10-21 11:11:15 +0100

    removed default routes

--- cbr_user_session/fast_api/User_Session__Fast_API.py
+++ cbr_user_session/fast_api/User_Session__Fast_API.py
@@ -3,8 +3,9 @@ from cbr_user_session.fast_api.routes.Routes__Info      import Routes__Info
 
 
 class User_Session__Fast_API(Fast_API):
-    base_path  : str  = '/user-session'
-    enable_cors: bool = True
+    base_path      : str  = '/user-session'
+    enable_cors    : bool = True
+    default_routes : bool = False
 
     def setup_routes(self):
         self.add_routes(Routes__Info   )
