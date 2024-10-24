8249dab475f1a2208193a692ef21e69d5f96ae63
Dinis Cruz <dinis.cruz@owasp.org>
2024-10-24 13:59:22 +0100

    wired up create_demo_guests

--- cbr_user_session/fast_api/routes/Routes__Guests.py
+++ cbr_user_session/fast_api/routes/Routes__Guests.py
@@ -1,3 +1,4 @@
+from cbr_shared.cbr_backend.guests.Guests__Create__Many import Guests__Create__Many
 from cbr_shared.cbr_sites.CBR__Shared_Objects       import cbr_shared_objects
 from osbot_fast_api.api.Fast_API_Routes             import Fast_API_Routes
 from osbot_utils.decorators.methods.cache_on_self   import cache_on_self
@@ -10,6 +11,9 @@ class Routes__Guests(Fast_API_Routes):
     @cache_on_self
     def db_guests(self):
         return cbr_shared_objects.db_guests()
+
+    def create_demo_guests(self):
+       return Guests__Create__Many().create_demo_guests()
 
     def delete_all_guests(self):
         deleted_guests_ids = []
@@ -28,7 +32,8 @@ class Routes__Guests(Fast_API_Routes):
         return self.db_guests().db_guests__ids()
 
     def setup_routes(self):
-        self.add_route_get(self.delete_all_guests)
-        self.add_route_get(self.data             )
-        self.add_route_get(self.ids              )
+        self.add_route_get(self.create_demo_guests)
+        self.add_route_get(self.delete_all_guests )
+        self.add_route_get(self.data              )
+        self.add_route_get(self.ids               )
         return self
