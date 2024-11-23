9eb54e96ecad4636b37f2599e19323228eaa4359
Dinis Cruz <dinis.cruz@owasp.org>
2024-11-23 00:55:57 +0000

    refactored imports

--- cbr_user_session/fast_api/User_Session__Fast_API.py
+++ cbr_user_session/fast_api/User_Session__Fast_API.py
@@ -1,12 +1,4 @@
-from cbr_shared.config.Server_Config__CBR_Website import server_config__cbr_website
-from osbot_local_stack.local_stack.Local_Stack          import Local_Stack
-from cbr_user_session.User_Session__Config              import user_session__config
-from cbr_user_session.fast_api.routes.Routes__Guest     import Routes__Guest
-from cbr_user_session.fast_api.routes.Routes__Guests    import Routes__Guests
-from cbr_user_session.fast_api.routes.Routes__Session   import Routes__Session
 from osbot_fast_api.api.Fast_API                        import Fast_API
-from cbr_user_session.fast_api.routes.Routes__Info      import Routes__Info
-
 
 class User_Session__Fast_API(Fast_API):
     base_path      : str  = '/user-session'
@@ -14,16 +6,27 @@ class User_Session__Fast_API(Fast_API):
     default_routes : bool = False
 
     def setup(self):
+        from cbr_user_session.User_Session__Config import user_session__config
+
         user_session__config.setup()
         self.setup__local_stack()
         super().setup()
         return self
 
     def setup__local_stack(self):                       # pragma: no cover
+        from cbr_shared.config.Server_Config__CBR_Website import server_config__cbr_website
+        from osbot_local_stack.local_stack.Local_Stack import Local_Stack
+
         if server_config__cbr_website.cbr_config().use_local_stack():
             Local_Stack().activate()
 
     def setup_routes(self):
+        from cbr_user_session.fast_api.routes.Routes__Guest     import Routes__Guest
+        from cbr_user_session.fast_api.routes.Routes__Guests    import Routes__Guests
+        from cbr_user_session.fast_api.routes.Routes__Session   import Routes__Session
+        from cbr_user_session.fast_api.routes.Routes__Info      import Routes__Info
+
+
         self.add_routes(Routes__Info   )
         self.add_routes(Routes__Session)
         self.add_routes(Routes__Guest  )
